b9f6934b44bec0fe4a7a82a9a6386ca8bd802b87
Ayush5071 <ayusht5071@gmail.com>
2025-06-19 02:51:47 +0530

    Disable Redis integration by commenting out all Redis-related code

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,8 +1,8 @@
-import Redis from "ioredis"
-import dotenv from "dotenv"
+// import Redis from "ioredis"
+// import dotenv from "dotenv"
 
-dotenv.config()
+// dotenv.config()
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+// export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
 
-await redis.set("foo","bar");
+// await redis.set("foo","bar");
